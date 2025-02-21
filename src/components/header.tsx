e4337df6dbe0f66a02084f97b58237f66748e7a5
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-02-21 12:53:44 +0200

    style header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,6 +5,7 @@ import { getPayload } from "payload";
 import configPromise from "@payload-config";
 import { MainMenu, MenuItem, MobileMenu } from "./MainMenu";
 import SearchSidePanel from "./SearchPanel";
+import { UserIcon } from "@heroicons/react/24/outline";
 
 export default async function Header() {
   const t = await getTranslations();
@@ -18,12 +19,12 @@ export default async function Header() {
   });
 
   return (
-    <header>
+    <header className="sticky top-0 z-40 bg-stone-900">
       <div className="container mx-auto flex items-center justify-between px-4 py-4 xl:px-0">
         <div className="lg:hidden">
           <MobileMenu items={mainMenu.items as MenuItem[]} />
         </div>
-        <div className="lg:w-[200px]">
+        <div className="lg:w-[300px]">
           <Link href="/" className="text-xl font-bold">
             {t("meta.title")}
           </Link>
@@ -31,7 +32,13 @@ export default async function Header() {
         <div className="hidden lg:block lg:flex-1">
           <MainMenu items={mainMenu.items as MenuItem[]} />
         </div>
-        <div className="flex items-center justify-end gap-4 lg:w-[200px]">
+        <div className="flex items-center justify-end gap-8 lg:w-[300px]">
+          <div className="flex items-center gap-4">
+            <Link href="/login" className="flex items-center gap-2 text-sm font-medium uppercase">
+              <UserIcon className="h-5 w-5" />
+              Log in
+            </Link>
+          </div>
           <SearchSidePanel />
           <LanguageSwitcher />
         </div>
