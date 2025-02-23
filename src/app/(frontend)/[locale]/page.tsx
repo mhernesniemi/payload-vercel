cd80708057d641fe00c257a406f50f08a3af9615
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-02-23 14:09:03 +0200

    remove console logs

--- src/app/(frontend)/[locale]/page.tsx
+++ src/app/(frontend)/[locale]/page.tsx
@@ -1,19 +1,14 @@
-import { getServerSession } from "next-auth/next";
 import configPromise from "@payload-config";
 import { getPayload } from "payload";
 import FrontPageTemplate from "@/app/templates/FrontPageTemplate";
 import AuthProvider from "@/components/auth/AuthProvider";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
-import { GET as authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 type Params = Promise<{ locale: "fi" | "en" }>;
 
 export default async function Home({ params }: { params: Params }) {
   const { locale } = await params;
-  const session = await getServerSession(authOptions);
-
-  console.log("server session", session);
 
   const payload = await getPayload({
     config: configPromise,
