e201c62a7d9a36e3aa5667ef50c8105b88241913
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-03-01 00:21:20 +0200

    change icon

--- src/features/lexical/test-feature/icon.tsx
+++ src/features/lexical/test-feature/icon.tsx
@@ -3,14 +3,5 @@
 import React from "react";
 
 export const TestIcon: React.FC = () => {
-  return (
-    <svg viewBox="0 0 24 24" height="16" width="16" aria-hidden="true" focusable="false">
-      <path
-        d="M21 10.975V8a2 2 0 0 0-2-2h-6V4.688c.305-.274.5-.668.5-1.11a1.5 1.5 0 0 0-3 0c0 .442.195.836.5 1.11V6H5a2 2 0 0 0-2 2v2.998l-.072.005A.999.999 0 0 0 2 12v2a1 1 0 0 0 1 1v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5a1 1 0 0 0 1-1v-2a1 1 0 0 0-.928-.975zM5 8h14v3H5V8zm15 12H5v-5h15v5z"
-        fill="currentColor"
-      />
-      <circle cx="8" cy="16" r="1" fill="currentColor" />
-      <circle cx="16" cy="16" r="1" fill="currentColor" />
-    </svg>
-  );
+  return <span>🤖</span>;
 };
