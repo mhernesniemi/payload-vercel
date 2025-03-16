0f04e21755f23319ad95106d06f761ac93f97de7
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-03-16 23:25:29 +0200

    check if front page content empty

--- src/components/templates/FrontPageTemplate.tsx
+++ src/components/templates/FrontPageTemplate.tsx
@@ -9,7 +9,7 @@ export default function FrontPageTemplate({ content }: FrontPageTemplateProps) {
     <main id="main-content">
       {content.hero && <BlockRenderer blocks={content.hero} />}
       <div className="mx-auto max-w-screen-lg py-16">
-        <BlockRenderer blocks={content.content} />
+        {content.content && <BlockRenderer blocks={content.content} />}
       </div>
     </main>
   );
