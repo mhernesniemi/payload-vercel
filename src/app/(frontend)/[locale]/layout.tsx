13254e5a7edff9941d8ff6c29d52f8378d4c8be6
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-03-18 20:52:38 +0200

    Revert "test static layout"
    This reverts commit 3b50af49600319ead028d42453cd190896ea3759.

--- src/app/(frontend)/[locale]/layout.tsx
+++ src/app/(frontend)/[locale]/layout.tsx
@@ -1,9 +1,11 @@
-import Heading from "@/components/Heading";
+import { Footer } from "@/components/Footer";
+import { Toaster } from "@/components/Toaster";
 import { routing } from "@/i18n/routing";
 import { SITE_NAME } from "@/lib/constants";
 import type { Metadata } from "next";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
 import { Inter } from "next/font/google";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import "./globals.css";
 
@@ -22,52 +24,23 @@ export const metadata: Metadata = {
   title: SITE_NAME,
 };
 
-export default async function RootLayout({ params }: Props) {
+export default async function RootLayout({ children, params }: Props) {
   const { locale } = await params;
 
   if (!routing.locales.includes(locale)) {
     notFound();
   }
 
+  const messages = await getMessages();
+
   return (
     <html lang={locale}>
       <body className={`${inter.className} flex min-h-screen flex-col bg-stone-900 text-white`}>
-        <div className="flex-grow">
-          <Heading size="xl" level="h1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-          </Heading>
-          <Image src="/placeholder-img.png" alt="Hero" width={1000} height={1000} />
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quisquam, quos.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quisquam, quos.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quisquam, quos.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quisquam, quos.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quisquam, quos.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Quisquam, quos. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Quisquam, quos.
-          </p>
-        </div>
+        <NextIntlClientProvider messages={messages}>
+          <div className="flex-grow">{children}</div>
+        </NextIntlClientProvider>
+        <Footer />
+        <Toaster />
       </body>
     </html>
   );
