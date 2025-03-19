9b3aec9eaf43bd8581295daeb39a161ea151d175
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-03-19 13:35:30 +0200

    optimise prod build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
-import { withPayload } from '@payloadcms/next/withPayload'
-import createNextIntlPlugin from 'next-intl/plugin'
+import { withPayload } from "@payloadcms/next/withPayload";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin()
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
-}
+  swcMinify: true,
+  compress: true,
+  images: { unoptimized: false },
+};
 
-export default withNextIntl(withPayload(nextConfig))
+export default withNextIntl(withPayload(nextConfig));
