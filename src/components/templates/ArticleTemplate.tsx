b607937d25d8bec08c41298dd840575452f65b05
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-03-27 00:47:11 +0200

    adjust article margins

--- src/components/templates/ArticleTemplate.tsx
+++ src/components/templates/ArticleTemplate.tsx
@@ -3,6 +3,7 @@ import { formatDateShort } from "@/lib/utils";
 import { Article } from "@/payload-types";
 import { useLocale } from "next-intl";
 import Image from "next/image";
+import Heading from "../Heading";
 
 interface ArticleTemplateProps {
   article: Article;
@@ -11,19 +12,21 @@ interface ArticleTemplateProps {
 export default function ArticleTemplate({ article }: ArticleTemplateProps) {
   const locale = useLocale();
   return (
-    <main id="main-content" className="mx-auto max-w-[800px] py-16">
+    <main id="main-content" className="mx-auto max-w-[800px] py-12">
       {typeof article.image === "object" && article.image?.url && (
         <Image
           src={article.image.sizes?.large?.url || article.image.url}
           alt={article.image.alt || ""}
           width={1920}
           height={1080}
-          className="mb-8 aspect-video w-full max-w-[800px] rounded-lg object-cover"
+          className="mb-10 aspect-video w-full max-w-[800px] rounded-lg object-cover"
           priority
         />
       )}
-      <h1 className="mb-4 text-4xl font-bold">{article.title}</h1>
-      <div className="mb-8 flex gap-4 text-stone-400">
+      <Heading level="h1" size="lg" className="mb-6">
+        {article.title}
+      </Heading>
+      <div className="mb-12 flex gap-4 text-sm text-stone-400">
         <time dateTime={article.publishedDate || ""}>
           {formatDateShort(article.publishedDate || "", locale)}
         </time>
@@ -34,7 +37,7 @@ export default function ArticleTemplate({ article }: ArticleTemplateProps) {
           </>
         )}
       </div>
-      <div className="mx-auto mt-12 max-w-screen-lg">
+      <div className="mx-auto max-w-screen-lg">
         <BlockRenderer nodes={article.content?.root?.children as NodeTypes[]} />
       </div>
     </main>
