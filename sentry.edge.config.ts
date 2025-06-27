4ef47c919ce2ba0adf6c37a2120ab910743ef630
Matti Hernesniemi <matti.hernesniemi@exove.fi>
2025-06-28 01:37:46 +0300

    test deploy

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -11,6 +11,6 @@ Sentry.init({
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
